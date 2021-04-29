ffdfd28b3d0fd664ffc736506c7de1414de3d921
gym315 <71267287+gym315@users.noreply.github.com>
2021-04-29 16:42:50 +0900

    Add files via upload

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -16,3 +16,28 @@ searchInputEl.addEventListener('blur', function () {
   searchInputEl.setAttribute('placeholder', '');
 });
 
+
+
+const badgeEl = document.querySelector('header .badges');
+
+//스크롤 이벤트
+window.addEventListener('scroll',_.throttle(function () {
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    //배지숨기기
+   // gsap.to(요소, 지속시간, 옵션);
+   gsap.to(badgeEl, .6, {
+     opacity: 0,
+     display: 'none'
+
+   });
+  } else {
+    //배지보이기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'none'
+    });
+
+  }
+}, 300));
+//_.thottle(함수, 시간)
